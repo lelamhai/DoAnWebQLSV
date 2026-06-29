0031caf4dc26b903540a84fe7f0d9d0c67384fcf
ADMIN\ADMIN <leelamhair@gmail.com>
2026-06-29 23:35:22 +0700

    i don't know

--- DoAnWebQLSV/Controllers/StudentScoreController.cs
+++ DoAnWebQLSV/Controllers/StudentScoreController.cs
@@ -1,12 +1,108 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DoAnWebQLSV.Models.Account;
+using DoAnWebQLSV.Models.Student;
+using DoAnWebQLSV.Models.Teacher;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace DoAnWebQLSV.Controllers
 {
     public class StudentScoreController : Controller
     {
-        public IActionResult Index()
+        private const string API_GETACCOUNTINFO = "https://localhost:7141/api/v1/private/Account/info-account?username=";
+        private const string API_GETSTUDENTSCORE = "https://localhost:7141/api/v1/private/Student/get-point-student";
+        public async Task<IActionResult> Index(int page = 1)
         {
+            var accessToken = HttpContext.Session.GetString("AccessToken");
+            var username = HttpContext.Session.GetString("Username");
+
+            if (string.IsNullOrWhiteSpace(accessToken))
+            {
+                TempData["LoginRequired"] = "Vui lòng đăng nhập để xem dữ liệu.";
+                return RedirectToAction("Index", "Login");
+            }
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", accessToken);
+            try
+            {
+                var accountInfo = await GetAccountInfoAsync(client, username);
+                var points = await GetStudentsAsync(client, username, page);
+                ViewBag.AccountInfo = accountInfo?.Data;
+                ViewBag.Points = points;
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = $"Lỗi khi lấy thông tin tài khoản: {ex.Message}";
+                return RedirectToAction("Index", "Login");
+            }
+
             return View();
         }
+
+        private async Task<AccountInfoViewModel?> GetAccountInfoAsync(HttpClient client, string? username)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                return null;
+            }
+
+
+            var url = API_GETACCOUNTINFO + username;
+
+            var response = await client.GetAsync(url);
+            var responseText = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"API tài khoản lỗi {(int)response.StatusCode}: {responseText}");
+            }
+
+            if (string.IsNullOrWhiteSpace(responseText))
+            {
+                return null;
+            }
+
+            var result = JsonSerializer.Deserialize<AccountInfoViewModel>(
+                responseText,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }
+            );
+
+            return result;
+        }
+
+        private async Task<PointStudentData> GetStudentsAsync(HttpClient client, string username, int page)
+        {
+            try
+            {
+                var url = API_GETSTUDENTSCORE + $"?masv={username}&page={page}";
+                var response = await client.GetAsync(url);
+                var responseText = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"API lớp lỗi {(int)response.StatusCode}: {responseText}");
+                }
+
+                var result = JsonSerializer.Deserialize<ListPointStudent>(
+                    responseText,
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    }
+                );
+
+                return result?.Data ?? new PointStudentData();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Lỗi khi gọi API sinh viên: {e.Message}");
+            }
+        }
+
     }
 }
---
+++ DoAnWebQLSV/Models/Student/PointStudentModel.cs
@@ -0,0 +1,45 @@
+﻿using DoAnWebQLSV.Models.Classroom;
+using DoAnWebQLSV.Models.Teacher;
+using System.Text.Json.Serialization;
+
+namespace DoAnWebQLSV.Models.Student
+{
+    public class ListPointStudent
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = string.Empty;
+
+        [JsonPropertyName("data")]
+        public PointStudentData Data { get; set; } = new PointStudentData();
+    }
+
+    public class PointStudentData
+    {
+        [JsonPropertyName("items")]
+        public List<PointStudentItem> Items { get; set; } = new List<PointStudentItem>();
+
+        [JsonPropertyName("pagination")]
+        public Pagination Pagination { get; set; } = new Pagination();
+    }
+
+    public class PointStudentItem
+    {
+        public string? MaMH { get; set; }
+
+        public string? TenMH { get; set; }
+
+        public int SoTinChi { get; set; }
+
+        public string? NienKhoa { get; set; }
+
+        public int HocKy { get; set; }
+
+        public decimal? DiemCC { get; set; }
+
+        public decimal? DiemGK { get; set; }
+
+        public decimal? DiemCK { get; set; }
+
+        public decimal? DiemTong { get; set; }
+    }
+}
